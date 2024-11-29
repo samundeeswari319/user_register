519ac5ff90b5f70123ff0bcadb4fa9bcf3ff50e7
novosoftsolutions <leviaugustin.g@novosoftsolutions.in>
2024-11-29 18:10:03 +0530

    mid and app_id based filter end point added

---
+++ src/main/java/com/merchant/register/model/UserFetchRequest.java
@@ -0,0 +1,12 @@
+package com.merchant.register.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserFetchRequest {
+    private String mid;
+    private String app_id;
+}
--- src/main/java/com/merchant/register/repository/UserRepository.java
+++ src/main/java/com/merchant/register/repository/UserRepository.java
@@ -1,10 +1,18 @@
 package com.merchant.register.repository;
 
 import com.merchant.register.model.User;
-import com.merchant.register.model.UserRegisterData;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
-import java.util.HashMap;
-
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
+
+    @Query("{ 'mid': ?0 }")
+    List<User> findByMid(String mid);
+
+    @Query("{ 'mid': ?0, 'app_id': ?1 }")
+    List<User> findByMidAndAppId(String mid, String app_id);
+
 }
